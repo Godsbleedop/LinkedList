0aa2ea1ae0df0efce4fb5b3d68b6b551ab03f0fc
Aaron Quadras <121336532+Godsbleedop@users.noreply.github.com>
2025-02-13 15:32:44 +0530

    Create plate_stacking.java

---
+++ plate_stacking.java
@@ -0,0 +1,8 @@
+// plate stacking in a restuarant
+/* a restaurant stacks plates on top off each other the waiter always takes the top plate first when surveying 
+implement a stack that allows 
+add a plate to the stack 
+2* remove and return the  top plate 
+3* see the top plate without removing it 
+4* check if there are any plates left 
+*/
